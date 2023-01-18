754aa504d624aa7143d16ed80539dbb3b489e531
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2023-01-18 16:05:10 +0530

    Image and Post Card Rnedered Lec: 36:55

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { Container , AppBar , Typography , Grow ,Grid }from '@mui/material'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
 const App = () => {
   return (
-    <div>App</div>
+    <Container maxwidth = "lg">
+      <AppBar position="static" color = 'inherit'>
+      <Typography variant ='h2' align = 'center'>Memories</Typography>
+      <img src={memories} alt="memories" heigth = '60' />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify = 'space-between' alignItems= 'stretch' spacing={4}>
+            <Grid item xs = {12} sm={7}>
+              <Posts/>
+            </Grid>
+            <Grid item xs = {12} sm={7}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+
+      </Grow>
+
+    </Container>
   )
 }
-
+// npm install @mui/material @emotion/react @emotion/styled
 export default App
