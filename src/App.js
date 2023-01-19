1f211204b1d33639be4d54856d576db7380a5f05
hitaishkumar <74085141+hitaishkumar@users.noreply.github.com>
2023-01-19 12:23:56 +0530

    Doubts File ++ , Styles ->Inline: Working 39:09

--- src/App.js
+++ src/App.js
@@ -3,13 +3,23 @@ import { Container , AppBar , Typography , Grow ,Grid }from '@mui/material'
 import memories from './images/memories.png'
 import Posts from './components/Posts/Posts'
 import Form from './components/Form/Form'
+// import styledd from './styles'
 const App = () => {
-
+  // const classes = styledd()
   return (
     <Container maxwidth = "lg">
-      <AppBar position="static" color = 'inherit'>
-          <Typography  variant ='h2' align = 'center'>Memories</Typography>
-          <img  src={memories} alt="memories" height="500" />
+      <AppBar  style={{
+          borderRadius: 15,
+          margin: "30px 0",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+         position="static" color = 'inherit'>
+          <Typography color = 'rgba(0,183,255, 1)'variant ='h2' align = 'center'>Memories</Typography>
+          <img style = {{ width : 50, height : 50, marginLeft : 10 }} src={memories} alt="memories"  />
+          {/*                                      ^ here margin is given bcoz makestyles hook is deprecated */}
       </AppBar>
       <Grow in>
         <Container>
--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,19 @@
-import { makeStyles } from "@mui/material";
+ 
+// ❌import { makeStyles } from "@mui/material/styles"; // <--- delete this ❌
 
-export default makeStyles(()=>({
+// import { styled } from "@mui/system"; // <--- Add this ✅
+
+// ❌export default makeStyles(() => ({ //  <--- delete this ❌
+
+// export default styled(()=>({ // <--- Add this ✅
+
+// Reason:
+
+// The @mui/styles package is not compatible with React 18,
+
+import { styled } from "@mui/system";
+
+export default styled(()=>({
     appBar: {
         borderRadius: 15,
         margin: '30px 0',
